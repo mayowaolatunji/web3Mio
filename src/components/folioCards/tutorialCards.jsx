ee146e0beb70631dfde36cbfa57fa936a8523477
mayowaolatunji <olatunjimayowa0396@gmail.com>
2024-02-03 02:02:43 +0100

    deployment

--- src/components/folioCards/tutorialCards.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './tutorialCards.css'
-
-function Tutorial() {
-  return <div className="Notice">TutorialCards: Coming Soon!!</div>;
-}
-
-export default Tutorial
